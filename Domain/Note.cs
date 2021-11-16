2b2ef19dd0255eef18e0151903267ffa949ee72f
Vitaliy <vitalik.vinnik.05@gmail.com>
2021-11-17 01:10:41 +0200

    added domain models

---
+++ Domain/Note.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Details { get; set; }
+        [Required]
+        public DateTime CreationDate { get; set; }
+        public DateTime? EditDate { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        public User User { get; set; }
+    }
+}
---
+++ Domain/User.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+        public ICollection<Note> Notes { get; set; }
+    }
+}
